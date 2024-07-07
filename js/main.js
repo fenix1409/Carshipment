ae9ce9ac37da6d210235746eecdf34dddb99706d
fenix1409 <b2550486@gmail.com>
2024-07-07 16:38:08 +0500

    Add mixin, shipping

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
      let elDropdownHead = document.querySelector (".dropdown__heading")
      let elDropdownHideContent = document.querySelector (".dropdown__hide-content")
      let elLine2 = document.querySelector (".line2")
+
      
      elDropdownHead. addEventListener("click", function(){
         elDropdownHideContent.classList.toggle("show")
