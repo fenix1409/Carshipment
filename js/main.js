253f19692adcc76c85bf0ec3eae90fecdf6b26a1
fenix1409 <b2550486@gmail.com>
2024-07-07 13:39:50 +0500

    Add block, scssr

---
+++ js/main.js
@@ -0,0 +1,8 @@
+     let elDropdownHead = document.querySelector (".dropdown__heading")
+     let elDropdownHideContent = document.querySelector (".dropdown__hide-content")
+     let elLine2 = document.querySelector (".line2")
+     
+     elDropdownHead. addEventListener("click", function(){
+        elDropdownHideContent.classList.toggle("show")
+        elLine2.classList.toggle("rotate")
+    })
